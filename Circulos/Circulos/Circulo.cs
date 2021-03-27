c94793ba7a1c2f3176be141409c892c1999daa91
SaulSlatt <81392882+SaulSlatt@users.noreply.github.com>
2021-03-27 15:22:54 -0600

    Add files via upload

---
+++ Circulos/Circulos/Circulo.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: DIANA
+ * Date: 26/03/2021
+ * Time: 05:25 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+
+namespace Circulos
+{
+	/// <summary>
+	/// Description of Circulo.
+	/// </summary>
+	public class Circulo
+	{
+		
+		public Circulo(int rad, int x,int y,int diam)
+		{	
+		}
+		
+		private int radioCirculo;
+		    public int RadioCirculo{
+			     get {return radioCirculo;}
+			     set {radioCirculo = value;}
+			}
+		private int XCirculo;
+			public int xCirculo{
+				get {return xCirculo;}
+			    set {xCirculo = value;}
+			}
+		private int YCirculo;
+			public int yCirculo{
+				get {return yCirculo;}
+			    set {yCirculo = value;}
+			}
+		private int DiametroCirculo;
+			public int diametroCirculo{
+				get {return diametroCirculo;}
+			    set {diametroCirculo = value;}
+			}
+			
+	}
+}
---
+++ Circulos/Circulos/MainForm.cs
@@ -0,0 +1,138 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: DIANA
+ * Date: 16/03/2021
+ * Time: 05:20 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace Circulos
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		
+		}
+		
+		int yi,xi,yf,xf;
+		int yc=0,xc=0;
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			openFileDialog1.ShowDialog();
+			pictureBoximage.Image = Image.FromFile(openFileDialog1.FileName);
+		}
+		
+
+		
+		void ButtonAnalizarClick(object sender, EventArgs e){
+		
+			int x=0,y=0;
+			
+			Bitmap bmpcentro = new Bitmap(openFileDialog1.FileName);
+			Graphics graf = Graphics.FromImage(bmpcentro);
+			Pen pen = new Pen(Color.Violet,3);
+			Color colory;
+			pictureBoximage.Image = bmpcentro;
+			
+				for( x=0; x < bmpcentro.Height; x++)
+					for( y=0; y < bmpcentro.Width; y++){//recorrer todos los pixeles
+					    colory=bmpcentro.GetPixel(y,x);
+					    if(colory.R!=255 && colory.G!=255&&colory.B!=255){//verificar que no sea blanco
+		                   BuscaCentro(y,x,bmpcentro);
+		                 
+		                   Circulo circulos = new Circulo();
+		                    
+		                   int ra = xc+xi;
+		                   int di = xi+xf;
+		                    List<Circulo> Lista = new List<Circulo>(){
+		                   	Lista.Add(Circulo(ra,xc,yc,di));
+		                  }	
+		                 
+		                          
+					}
+					
+					    foreach(Circulo c in Lista){//recorrer la lista
+				            label1.Text = ("Centro: "+xc.ToString()+ ","+yc.ToString());
+				    }
+						
+		     }
+		}//BuscarCentro
+		
+	
+		void BuscaCentro(int x,int y,Bitmap image){
+		
+		Color pixcol;
+		pixcol=image.GetPixel(x,y);
+				       yi=y;//Y inicial
+				       yf=y;//y final
+				       //MITAD DEL CIRCULO
+				       while(pixcol.R!=255 && pixcol.G!=255&& pixcol.B!=255){
+				       	  pixcol=image.GetPixel(x,yf);
+				      	  yf++;
+				      	  
+						}
+						
+				       yc=(y+yf)/2;
+						//X CENTRAL
+						xi=x;
+						xf=x;
+						while(pixcol.R!=255 && pixcol.G!=255&& pixcol.B!=255){
+							pixcol=image.GetPixel(xi,yc);
+							xi--;
+							
+						}
+						
+						while(pixcol.R!=255 && pixcol.G!=255&& pixcol.B!=255){
+							pixcol=image.GetPixel(xf,yc);
+							xf++;
+						}
+					
+						xc=(xi+xf)/2;//x central
+	
+		}//point
+		
+		
+		
+		void dibujacirculo(int f,int c){
+			
+		}
+	
+		
+	}
+	
+}
+/*
+
+			Bitmap bmp = new Bitmap(openFileDialog1.FileName);
+			Color colorActual;
+			pictureBoximage.Image = bmp;
+			
+			for(int i=0; i < bmp.Height; i++)
+				for(int j=0; j < bmp.Width; j++){
+				
+				   colorActual = bmp.GetPixel(j,i);
+			       if(colorActual.R!=255)
+			          if(colorActual.G!=255)
+					     if(colorActual.B!=255)
+						    bmp.SetPixel(j,i,Color.Blue);
+			}
+		*/	
